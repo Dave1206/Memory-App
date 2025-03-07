2265b3e09a7af312b8df0145fbef93f30f88e6ca
Dave1206 <davidw120691@gmail.com>
2025-03-07 15:48:04 -0500

    removing debug logs and comments

--- server/server.js
+++ server/server.js
@@ -104,7 +104,6 @@ passport.use(
         const result = await db.query("SELECT * FROM users WHERE email = $1", [email]);
         if (result.rows.length > 0) {
           const user = result.rows[0];
-          console.log("passport log:", user);
           const isValid = await bcrypt.compare(password, user.password);
           if (isValid) {
             return done(null, user);
